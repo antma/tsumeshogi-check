fb8f09276b4b209e50a715a712cb870f5f086c1f
antma <antma@users.noreply.github.com>
2023-03-14 22:08:30 +0300

    psn::parse_header

--- src/psn.rs
+++ src/psn.rs
@@ -2,6 +2,68 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+#[test]
+fn test_parse_header() {
+  assert_eq!(
+    parse_header("[Date \"24/02/2023\"]"),
+    Some((String::from("date"), String::from("24/02/2023")))
+  );
+}
+
+fn parse_header(s: &str) -> Option<(String, String)> {
+  let mut key = String::new();
+  let mut value = String::new();
+  let mut st = 0;
+  for c in s.trim().chars() {
+    match st {
+      0 => {
+        if c == '[' {
+          st += 1;
+        } else {
+          return None;
+        }
+      }
+      1 => {
+        if c.is_ascii_whitespace() {
+          st += 1;
+        } else if c.is_ascii_alphabetic() {
+          key.push(c.to_ascii_lowercase());
+        } else {
+          return None;
+        }
+      }
+      2 => {
+        if !c.is_ascii_whitespace() {
+          if c == '"' {
+            st += 1;
+          } else {
+            return None;
+          }
+        }
+      }
+      3 => {
+        if c == '"' {
+          st += 1;
+        } else {
+          value.push(c);
+        }
+      }
+      4 => {
+        if c == ']' {
+          st += 1;
+        } else {
+          return None;
+        }
+      }
+      _ => return None,
+    }
+  }
+  if st < 5 {
+    return None;
+  }
+  Some((key, value))
+}
+
 struct PSNFileIterator {
   reader: BufReader<File>,
 }
