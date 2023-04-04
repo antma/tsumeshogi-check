b30d87be73446f4f84a9c5d89441b96bbc7b0719
antma <antma@users.noreply.github.com>
2023-04-04 16:53:15 +0300

    bugfix: cell::between()

--- src/shogi/cell.rs
+++ src/shogi/cell.rs
@@ -35,7 +35,7 @@ pub fn delta_direction(cell1: usize, cell2: usize) -> Direction {
 }
 pub fn between(cell1: usize, cell2: usize) -> u128 {
   let mut r = 0u128;
-  let (delta_row, delta_col) = delta(cell1, cell2);
+  let (delta_row, delta_col) = delta_direction(cell1, cell2);
   let delta = 9 * delta_row as isize + delta_col;
   let mut k = cell1;
   loop {
@@ -46,4 +46,12 @@ pub fn between(cell1: usize, cell2: usize) -> u128 {
     r |= 1u128 << k;
   }
   r
+}
+
+#[test]
+fn test_between() {
+  assert_eq!(
+    between(3, 9 * 3 + 0),
+    (1u128 << (9 * 2 + 1)) + (1u128 << (9 * 1 + 2))
+  );
 }
