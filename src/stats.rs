a2df5dd2dccf6491641e760091c379bdbe7eaa6e
antma <antma@users.noreply.github.com>
2023-09-06 16:56:27 +0300

    struct stats::Average

--- src/stats.rs
+++ src/stats.rs
@@ -1,3 +1,34 @@
+use std::fmt;
+
+#[derive(Default)]
+pub struct Average {
+  sum: u64,
+  total: u64,
+}
+
+impl std::ops::AddAssign<u64> for Average {
+  fn add_assign(&mut self, rhs: u64) {
+    self.sum += rhs;
+    self.total += 1;
+  }
+}
+
+impl Average {
+  fn average(&self) -> f64 {
+    if self.total == 0 {
+      0.0
+    } else {
+      self.sum as f64 / self.total as f64
+    }
+  }
+}
+
+impl fmt::Debug for Average {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{:.10}", self.average())
+  }
+}
+
 #[cfg(feature = "stats")]
 macro_rules! incr {
   ($e:expr) => {
